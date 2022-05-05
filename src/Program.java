df5672791c46e68d5e0bcbfa4190dc145048a5ac
azmatalikhan <azmatalikhan@gmail.com>
2022-05-05 19:59:03 +0200

    arsalan first feature implemented

---
+++ src/Program.java
@@ -0,0 +1,11 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		System.out.println("ALLAH is the Greatest!");
+		System.out.println("Azmat added line 1");
+		System.out.println("Azmat added line 2");
+		System.out.println("Arsalan first main commit!");
+	}
+
+}
